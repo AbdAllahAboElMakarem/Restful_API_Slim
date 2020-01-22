ff1a3a86dc79dc0c67d2874590710ef245a97151
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2020-01-22 14:04:00 +0200

    tast2

--- public/index.php
+++ public/index.php
@@ -13,19 +13,26 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response;
 });
 
-$app->get('/id/{name}', function (Request $request, Response $response, array $args) {
-    $id = $args['name'];
+$app->get('/id/{id}', function (Request $request, Response $response, array $args) {
+    $id = $args['id'];
     $response->getBody()->write("Hello, $id");
     return $response;
 });
 
-$app->post('/tast/name', function (Request $a1, Request $a2){
+$app->post('/tast/{name}', function (Request $a1, Request $a2){
 $data=$a1->getParsedBody();
 $inputdata=[];
 $inputdata['name']=filter_var($data['name'], FILTER_SANITIZE_STRING);
 $inputdata['id']=filter_var($data['id'], FILTER_SANITIZE_STRING);
 $a2->getBody()->write('daer ' .$inputdata['name'].'your phone unmber is ' .$inputdata['phone']);
 
+});
+
+$app->get('/testargs/{Name}/{phone}', function ($request,$response, array $args) {
+    $Name = $args['Name'];
+    $phone = $args['phone'];
+    $response->getBody()->write("thes is a test for args, $Name you phone numbor is $phone");
+    return $response;
 });
 
 

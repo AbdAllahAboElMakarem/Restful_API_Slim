36529031ee1ee0563debf2b4bc57397d52fd358c
AbdAllahAboElMakarem <abdallah.m.daly@gmail.com>
2020-01-23 23:51:10 +0200

    optional

--- public/index.php
+++ public/index.php
@@ -82,4 +82,52 @@ if ($request->isGet()){
 }
 });
 
+$app->get('/password[/{id}]' ,function($requ ,$resp ,$argc){
+$id=$argc['id'];
+if (is_null(id)){
+$requ->getBody()->write('Ths id is null');
+}
+else{
+$resp->getBody()->write('THS $id=id');
+}
+
+});
+
+
+$app->get('/password[/{year}[/{month}]]' ,function($requ ,$resp ,$argc){
+
+$year=$argc['year'];
+$month=$argc['month'];
+
+
+if (is_null(year)){
+$requ->getBody()->write('Ths year and month are is null');
+
+}
+else{
+if(is_null($month)){
+
+$requ->getBody()->write('Ths year=$year month are is null');
+}
+else{
+$requ->getBody()->write('Ths year=$year month=$month');
+}}
+
+});
+$app->get('/unlimited/optional[/{parms:.*}]',function($req ,$res ,$args){
+$parms=explode('/', $req->getAttribute('parms'));
+        
+if (empty($parms[0])){
+$res->getBody()->write("The parms list is null");
+}
+else{
+$out="";
+foreach ($parms as $key => $value) {
+$out=$out." " ."$key => $value";
+}
+$res->getBody()->write($out);	
+}
+});
+
+
 $app->run();
